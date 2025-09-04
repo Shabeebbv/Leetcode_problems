7610328641ecf8ddb2f1ca02488d6a06ef797dd7
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-09-04 12:40:15 +0530

    first commit

---
+++ leetcode 12.js
@@ -0,0 +1,8 @@
+// Array.prototype.last = function() {
+    
+Array.prototype.last = function() {
+    //   num=([1,2,3])
+    return  length-1
+}
+    let num=([1,2,3])
+    console.log(num.last())

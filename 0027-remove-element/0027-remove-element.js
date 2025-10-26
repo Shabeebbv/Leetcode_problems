733a1fc4960534d809c410ad748717163d373ffc
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-10-26 19:38:48 +0530

    Time: 0 ms (100%), Space: 54.3 MB (46.29%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,19 @@
+var removeElement = function(nums, val) {
+
+    k=0
+
+    for(i=0;i<nums.length;i++){
+       if(nums[i]!==val){
+        nums[k]=nums[i]
+        k++
+
+       }
+      
+    }
+return k
+
+
+    
+};
+const x=removeElement([0,1,2,2,3,0,4,2],2)
+console.log(x);

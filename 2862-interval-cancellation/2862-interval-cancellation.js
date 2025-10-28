aa62fa6836081f0f4aff3be9aa0d0d52caed132d
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-10-28 11:25:19 +0530

    Time: 55 ms (72.19%), Space: 55.1 MB (22.04%) - LeetHub

---
+++ 2862-interval-cancellation/2862-interval-cancellation.js
@@ -0,0 +1,9 @@
+var cancellable = function(fn, args, t) {
+    fn(...args)
+    const timer = setInterval(() => {
+        fn(...args);
+    }, t);
+
+    const cancelFn = () => clearInterval(timer);
+    return cancelFn;
+};

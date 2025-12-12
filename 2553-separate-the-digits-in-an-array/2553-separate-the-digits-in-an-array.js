f644ea71e6525e078da0265b17340edb2c75904f
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-12-12 09:36:55 +0530

    Time: 3 ms (81.32%), Space: 58.2 MB (63.74%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var separateDigits = function(nums) {
+    return nums.join('').split('').map(Number)
+};
+console.log(separateDigits([13,25,83,77]));

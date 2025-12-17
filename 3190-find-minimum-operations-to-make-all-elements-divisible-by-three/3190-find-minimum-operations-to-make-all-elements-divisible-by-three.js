7887f9a06b5a9984071d75dbf26f6ea3d9a6125e
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-12-17 09:37:10 +0530

    Time: 2 ms (5.88%), Space: 54.2 MB (97.85%) - LeetHub

---
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function(nums) {
+  let count=0
+  for(i=0;i<nums.length;i++){
+    if(nums[i]%3!=0){
+      count++
+    }
+  }
+  return count
+    
+};
+console.log(minimumOperations([1,2,3,4]));

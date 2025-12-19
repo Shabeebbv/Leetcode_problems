d70b4c4ad44d93e36678b18682a18aeaca133aff
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-12-19 10:41:20 +0530

    Time: 109 ms (24.07%), Space: 65.3 MB (62.96%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.js
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.js
@@ -3,21 +3,15 @@
  * @return {number}
  */
 var maxCoins = function(piles) {
-    let sum=0
-    let count=piles.length/3
-    let x=count
-    console.log(count);
-    
-    let srt=piles.sort((x,y)=>x-y)
-    console.log(srt);
-    
-    for(i=0;i<piles.length;i++){
-      if(i>=count && count>0){
-        sum+=srt[x]
-        x+=2
-        count--
-      }
+    piles.sort((a,b)=>a-b)
+    let left=0
+    let right=piles.length-1
+    let total=0;
+    while(left<right){
+        right--
+        total+=piles[right]
+        right--
+        left++
     }
-    return sum
+    return total
 };
-console.log(maxCoins([9,8,7,6,5,1,2,3,4]));

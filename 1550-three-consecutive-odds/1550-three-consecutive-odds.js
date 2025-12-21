9e663713b259a62853a63c9bcf50c7f6047a5cc7
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-12-21 12:57:35 +0530

    Time: 1 ms (21.62%), Space: 54.1 MB (71.17%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    let count=0
+    for(i=0;i<arr.length;i++){
+        if(arr[i]%2!=0){
+            count++
+            if(count==3){
+                return true
+            }
+        }
+        else{
+            count=0
+        }
+    }
+    return false
+};
+console.log(threeConsecutiveOdds([1,2,34,3,4,5,7,23,12]));

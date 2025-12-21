e22338863c9b8bb8d249a7348cf32b0d16e45e31
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-12-21 12:48:17 +0530

    Time: 1 ms (65.66%), Space: 55.6 MB (77.11%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.js
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.js
@@ -1,11 +1,11 @@
 var countKeyChanges = function(s) {
-    let count=0
+    let count=-1
     let up=s.toUpperCase()
     for(i=0;i<s.length;i++){
         if(up[i]!=up[i+1]){
             count++
         }
     }
-    return count-1
+    return count
 };
 console.log(countKeyChanges("aAbBcC"));

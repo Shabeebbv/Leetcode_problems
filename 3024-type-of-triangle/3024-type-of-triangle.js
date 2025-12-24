e89a14c705e3d6c66dcce067d825995cef4879e3
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-12-24 20:26:00 +0530

    Time: 0 ms (100%), Space: 55.4 MB (54.55%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.js
@@ -0,0 +1,18 @@
+var triangleType = function(nums) {
+    if(nums[0]+nums[1]<=nums[2]||nums[0]+nums[2]<=nums[1]||nums[1]+nums[2]<=nums[0]){
+        return 'none'
+    }
+        else{
+    if(nums[0]==nums[2]&&nums[1]==nums[0]){
+        return 'equilateral'
+    }
+    else if(nums[0]==nums[1]||nums[1]==nums[2]||nums[0]==nums[2]){
+        return 'isosceles'
+    }
+    else{
+        return 'scalene'
+    }
+    }
+
+}
+console.log(triangleType([3,3,3]));

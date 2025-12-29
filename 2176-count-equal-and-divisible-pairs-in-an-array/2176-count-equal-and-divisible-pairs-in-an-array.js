883c9c0ef8cb0356798aeb8aa37b195ecb3a90f5
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-12-29 16:58:32 +0530

    Time: 4 ms (35.97%), Space: 56.2 MB (48.2%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.js
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.js
@@ -1,17 +1,9 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
 var countPairs = function(nums, k) {
     let count=0
     for(i=0;i<nums.length;i++){
         for(j=i+1;j<nums.length;j++){
-            if(nums[i]==nums[j]){
-                let x=i*j
-                if(x%k==0){
+            if(nums[i]==nums[j] && (i*j)%k==0){
                     count++
-                }
             }
         }
     }

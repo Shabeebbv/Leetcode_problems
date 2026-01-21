bfc4ab50c7a1dfaa01244d6f5d5f6a6e25466dda
Mohammed shabeeb <sabishabeeb2@gmail.com>
2026-01-21 15:00:12 +0530

    Time: 0 ms (100%), Space: 55.6 MB (65.31%) - LeetHub

--- 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.js
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.js
@@ -4,12 +4,9 @@
  * @return {number}
  */
 var minOperations = function(nums, k) {
-    let srt=nums.sort((x,y)=>x-y)
     let num=0
-    for(i=0;i<srt.length;i++){
-        console.log(srt[i]);
-        
-        if(srt[i]<k){
+    for(i=0;i<nums.length;i++){
+        if(nums[i]<k){
             num++
         }
     }

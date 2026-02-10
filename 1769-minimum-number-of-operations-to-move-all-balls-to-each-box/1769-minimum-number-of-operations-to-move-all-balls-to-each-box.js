0416d468076e30e2756a7d516a156662e7cff067
Mohammed shabeeb <sabishabeeb2@gmail.com>
2026-02-10 11:00:01 +0530

    Time: 391 ms (18.08%), Space: 58.4 MB (53.67%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} boxes
+ * @return {number[]}
+ */
+var minOperations = function(boxes) {
+    let arr=[]
+    for(i=0;i<boxes.length;i++){
+        let count=0
+        for(j=0;j<boxes.length;j++){
+            if(boxes[j]>0 && j!=i){
+                count+=Math.abs(i-j)
+            }
+        }
+        arr.push(count)
+    }
+    return arr
+};
+console.log(minOperations('001011'));

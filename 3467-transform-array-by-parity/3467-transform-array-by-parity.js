73ae5baf686f134e1b69b4e6b66bf91b9fbbcc6e
Mohammed shabeeb <sabishabeeb2@gmail.com>
2026-02-20 15:03:19 +0530

    Time: 2 ms (68.1%), Space: 58 MB (38.35%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var transformArray = function(nums) {
+let arr=[]  
+for(i=0;i<nums.length;i++){
+    if(nums[i]%2==0){
+        arr.push(0)
+    }
+    else{
+        arr.push(1)
+    }
+}
+return arr.sort()
+};
+console.log(transformArray([1,5,1,4,2]));

073e036d822eb5daf5c7f2bebe20f37038e76c72
Mohammed shabeeb <sabishabeeb2@gmail.com>
2026-02-27 14:26:56 +0530

    Time: 1 ms (33.14%), Space: 57.5 MB (8.28%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isArraySpecial = function(nums) {
+    if(nums.length==1){
+        return true
+    }
+    for(i=0;i<nums.length-1;i++){
+        if(nums[i]%2==nums[i+1]%2){
+            return false
+        }
+    }
+    return true
+};
+console.log(isArraySpecial([4,3,1,6]));

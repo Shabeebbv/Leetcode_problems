286947e3ec5c8d590312231fdc4f5ea3e52f2229
Mohammed shabeeb <sabishabeeb2@gmail.com>
2026-03-11 10:13:48 +0530

    Time: 11 ms (46.87%), Space: 63 MB (18.9%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    let set1 = new Set(nums1)
+    let set2 = new Set(nums2)
+
+    let arr1=[]
+    let arr2=[]
+    
+    for(let x of set1){
+        if(!set2.has(x)){
+            arr1.push(x)
+        }
+    }
+    for(let y of set2){
+        if(!set1.has(y)){
+            arr2.push(y)
+        }
+    }
+    return [arr1,arr2]
+}
+console.log(findDifference([1,2,3,3],[1,1,2,2]))

8b7bacf2f523566acabbbf9e7f65a8f078db2426
Mohammed shabeeb <sabishabeeb2@gmail.com>
2026-04-11 10:41:27 +0530

    Time: 452 ms (5.09%), Space: 56.5 MB (39.95%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,12 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-  let arr=[]
     for(let i=0;i<numbers.length;i++){
       for(let j=i+1;j<numbers.length;j++){
         if(numbers[i]+numbers[j]==target){
-          arr.push(i+1,j+1)
-          return arr
+          return [i+1,j+1]
         }
       }
     }

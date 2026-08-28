aa216467f49ebdb45ded0fe971d770066ddfd0a1
Mohammed shabeeb <sabishabeeb2@gmail.com>
2026-08-28 15:24:00 +0530

    Time: 3 ms (11.37%), Space: 19.3 MB (23.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,5 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        n=len(nums)
+        for i in range(n):
+            for j in range(n-i-1):
+                if nums[j]>nums[j+1]:
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+        return nums
         
